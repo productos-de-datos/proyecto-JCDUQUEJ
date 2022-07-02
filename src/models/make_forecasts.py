ab1a8a82db9e482666b2cb78d29da263b8b22dda
JCDUQUEJ <101428664+JCDUQUEJ@users.noreply.github.com>
2022-07-02 00:21:56 -0500

    V1.3_07/02/2022

--- src/models/make_forecasts.py
+++ src/models/make_forecasts.py
@@ -1,6 +1,7 @@
 import pickle
 import csv
 import pmdarima
+from train_daily_model import read_data, spliting_data, trained_model
 
 def make_forecasts():
     """Construya los pronosticos con el modelo entrenado final.
@@ -17,21 +18,13 @@ def make_forecasts():
 
     """
 
-    from train_daily_model import read_data, spliting_data, trained_model
-
     daily_data = read_data()
     train, valid = spliting_data(daily_data)
     DT_Model = trained_model(train)
     y_pred_test = DT_Model.predict(1, len(daily_data))
-    predictions = predict_serie(y_pred_test, daily_data)
-    df_DT_model = concatenate_real_forecasts(daily_data, predictions)
+    df_DT_model = concatenate_real_forecasts(daily_data, y_pred_test)
     save_forecasts(df_DT_model)
 
-def predict_serie(y_pred_test, daily_data):
-    from train_daily_model import trained_model
-    y_pred_test = trained_model(daily_data)
-    predictions = y_pred_test.predict(1, len(daily_data), typ = 'levels').rename("Predictions")
-    return predictions
 
 def concatenate_real_forecasts(daily_data, predictions):
     df_DT_model = daily_data
