18933653511de67680ed6b7dc0803b8ef946b8f2
JCDUQUEJ <101428664+JCDUQUEJ@users.noreply.github.com>
2022-07-01 23:47:28 -0500

    V1.17_07/01/2022

--- src/models/make_forecasts.py
+++ src/models/make_forecasts.py
@@ -1,3 +1,5 @@
+import pickle
+
 def make_forecasts():
     """Construya los pronosticos con el modelo entrenado final.
 
@@ -12,10 +14,37 @@ def make_forecasts():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+
+    from train_daily_model import read_data, spliting_data
+
+    daily_data = read_data()
+    train, valid = spliting_data(daily_data)
+    DT_Model = load_pkl()
+    y_pred_test = prediction_test_model(DT_Model, daily_data)
+    df_DT_model = concatenate_real_forecasts(daily_data, y_pred_test)
+    save_forecasts(df_DT_model)
+
+def load_pkl():
+    with open("src/models/precios-diarios.pkl", "rb") as file:
+        DT_Model = pickle.load(file)
+    return DT_Model
+
+def prediction_test_model(DT_Model, daily_data):
+    # Prediccion de x_test
+    y_pred_test = [DT_Model.predict(x_test) for x_test in range(len(daily_data))]
+    return y_pred_test
+
+def concatenate_real_forecasts(daily_data, y_pred_test):
+    df_DT_model = daily_data
+    df_DT_model['Forecast'] = y_pred_test
+    return df_DT_model
+
+def save_forecasts(df_DT_model):
+    df_DT_model.to_csv('data_lake/business/forecasts/precios-diarios.csv', index=None)
 
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+    make_forecasts()
